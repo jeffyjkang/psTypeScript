0425ba71b75731d0466675e1c0038ebda7bcfe98
jeff kang <jeffyjkang@gmail.com>
2019-11-14 22:24:26 -0600

    init commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var VendingMaching = /** @class */ (function () {
+    function VendingMaching() {
+    }
+    return VendingMaching;
+}());
+/// <reference path="vendingMachine.ts" />
+var machine = new VendingMaching();
+//# sourceMappingURL=app.js.map
